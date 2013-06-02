cca3818c42b9c460802ae6f5f847a7e5d1091054
DSIW <dsiw@dsiw-it.de>
2013-06-02 17:02:56 +0200

    Add hashable module with specs

---
+++ lib/ry_safe/util/hashable.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+require 'digest'
+
+module RySafe::Util::Hashable
+  def hash
+    Digest::SHA1.hexdigest hash_data.to_s
+  end
+
+  protected
+
+  def hash_data
+    # should be overwritten after include
+  end
+end
