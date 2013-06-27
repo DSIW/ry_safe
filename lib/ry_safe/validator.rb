0b35eb70cb540ecff2dd6c910f1209b981aee3a9
DSIW <dsiw@dsiw-it.de>
2013-06-27 19:20:56 +0200

    Add validator with spec

---
+++ lib/ry_safe/validator.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+module RySafe
+  class Validator
+    def valid?
+      true
+    end
+  end
+end
---
+++ spec/ry_safe/validator_spec.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require "spec_helper"
+
+describe Validator do
+  subject { Validator.new }
+
+  it { should be_valid }
+end
