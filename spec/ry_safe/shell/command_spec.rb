573c94b8fec1efe2e0eaec2ba98c4af8e545059d
DSIW <dsiw@dsiw-it.de>
2013-06-27 21:52:47 +0200

    Add tree clearing before each test to command_spec

--- spec/ry_safe/shell/command_spec.rb
+++ spec/ry_safe/shell/command_spec.rb
@@ -2,29 +2,36 @@
 
 require "spec_helper"
 
-describe Command::Base do
-  subject { Command::Base.new("touch file") }
-
-  its(:command) { should == "touch" }
-  its(:arguments) { should == ["file"] }
-  its(:humand_readable_command) { should == "Touch" }
-
-  it "should call method action if " do
-    subject.should_receive(:action)
-    subject.call
+describe Command do
+  before do
+    Safe::Tree.root.clear
   end
-end
 
-describe Command::Touch do
-  subject { Command::Touch.new("file") }
+  describe Command::Base do
+    subject { Command::Base.new("touch file") }
 
-  its(:command) { should == "touch" }
-  its(:arguments) { should == ["file"] }
+    its(:command) { should == "touch" }
+    its(:arguments) { should == ["file"] }
+    its(:humand_readable_command) { should == "Touch" }
 
-  it "should create a new entry" do
-    tree = Safe::Tree.root
-    subject.call
-    tree.should have(1).node
-    tree.children.first.name.should == "file"
+    it "should call method action if " do
+      subject.should_receive(:action)
+      subject.call
+    end
+  end
+
+  describe Command::Touch do
+    subject { Command::Touch.new("file") }
+
+    its(:command) { should == "touch" }
+    its(:arguments) { should == ["file"] }
+
+    it "should create a new entry" do
+      tree = Safe::Tree.root
+      subject.call
+      tree.should have(1).node
+      tree.children.first.name.should == "file"
+      tree.children.first.should be_a Safe::Entry
+    end
   end
 end
