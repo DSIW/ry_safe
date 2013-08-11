0574dce062c7cb71359b4eba42e3edff344424ae
DSIW <dsiw@dsiw-it.de>
2013-08-11 18:19:55 +0200

    Fix spec for persistence prepare

--- spec/ry_safe/persistence/history_spec.rb
+++ spec/ry_safe/persistence/history_spec.rb
@@ -13,6 +13,7 @@ describe Persistence::History do
     it "should prepare base and touch file" do
       FileUtils.should_receive(:mkdir_p).with(Persistence::Base.new.location)
       FileUtils.should_receive(:touch).with(subject.location)
+      subject.should_receive(:load)
       subject.prepare
     end
   end
